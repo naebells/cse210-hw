1ec73ea2fcb0c9326f51fa7ea3d342000bd43843
Nathan Pack <nathanpack16@gmail.com>
2025-01-24 15:55:34 -0700

    Completed Prep 4's Activities.

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,40 @@
 using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        Console.WriteLine("Enter a list of numbers, type 0 when finished:");
+        string done = "no";
+        int count = 0;
+        while (done == "no")
+        {
+            Console.Write("Enter a Number: ");
+            string userInput = Console.ReadLine();
+            int number = int.Parse(userInput);
+            if (number != 0)
+            {
+                numbers.Add(number);
+                count += 1;
+                done = "no";
+            }
+            else if (number == 0)
+            {
+                done = "yes";
+
+                int sum = numbers.Sum();
+                Console.WriteLine($"Sum: {sum}");
+
+                float average = ((float)sum / count);
+                Console.WriteLine($"Average: {average}");
+
+                int max = numbers.Max();
+                Console.WriteLine($"Maximum: {max}");
+            }
+        }
     }
 }
