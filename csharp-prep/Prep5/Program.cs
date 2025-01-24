c166f603ed237f3f1200045af996b6329fe0d647
Nathan Pack <nathanpack16@gmail.com>
2025-01-24 16:09:56 -0700

    Completed Prep 5's associated activities.

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+
+        string userName = PromptUserName();
+        int userNumber = PromptUserNumber();
+
+        int userSquare = SquareNumber(userNumber);
+
+        DisplayResult(userName, userSquare);
+
+        static void DisplayWelcome()
+        {
+            Console.WriteLine("Welcome to the Program!");
+        }
+        static string PromptUserName()
+        { 
+            Console.Write("What is your name? ");
+            string userName = Console.ReadLine();
+            return userName;
+        }
+        static int PromptUserNumber()
+        {
+            Console.Write("What is your favorite number? ");
+            string userInput = Console.ReadLine();
+            int userNumber = int.Parse(userInput);
+            return userNumber;
+        }
+        static int SquareNumber(int userNumber)
+        {
+            int userSquare = (userNumber*userNumber);
+            return userSquare;
+        }
+        static void DisplayResult(string userName, int userSquare)
+        {
+            Console.Write($"Your name is {userName} and your favorite number squared is {userSquare}.");
+        }
     }
 }
